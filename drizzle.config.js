9aa419e2554ca1e042ead48b1e5953977b6330d7
roginand <roginandv@gmail.com>
2026-03-25 02:17:18 +0800

    chore: add drizzle configuration and update TypeScript module settings

---
+++ drizzle.config.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+    dialect: "postgresql",
+    schema: "./src/infrastructure/database/schema/*.ts",
+    out: "./drizzle",
+    dbCredentials: {
+        url: process.env.DATABASE_URL ?? "",
+    },
+    verbose: true,
+    strict: true,
+});
