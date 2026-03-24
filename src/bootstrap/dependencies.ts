84d656955664da3069c7ad2ded86be75e4dd0955
roginand <roginandv@gmail.com>
2026-03-25 02:18:13 +0800

    feat: add flashcardService to AppDependencies and update createDependencies function

--- src/bootstrap/dependencies.ts
+++ src/bootstrap/dependencies.ts
@@ -1,17 +1,22 @@
+import { createFlashcardsGenerationRunner } from "../agents/flashcards/agent";
+import { FlashcardService } from "../features/flashcards/flashcard.service";
 import { UserRepository } from "../features/users/user.repository";
 import { UserService } from "../features/users/user.service";
 import { db } from "../infrastructure/database/client";
 
 export interface AppDependencies {
+  flashcardService: FlashcardService;
   userRepository: UserRepository;
   userService: UserService;
 }
 
 export function createDependencies(): AppDependencies {
+  const flashcardService = new FlashcardService(createFlashcardsGenerationRunner());
   const userRepository = new UserRepository(db);
   const userService = new UserService(userRepository);
 
   return {
+    flashcardService,
     userRepository,
     userService,
   };
